a14c1da31453fb3a215fe530f453b76a38244f17
alexeyev <anton.m.alexeyev@gmail.com>
2016-08-08 00:33:14 +0300

    Parsing gramm info

--- model.py
+++ model.py
@@ -1,25 +1,40 @@
 from pymystem3 import Mystem
-import json
 import logging
+import re
 
-print "Loading mystem"
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(message)s',
+                    handlers=[logging.StreamHandler()])
+
+logging.info("Loading mystem")
 m = Mystem()
-
-print "Loaded mystem"
+logging.info("Loaded mystem")
 
 
 def parse_gr(gr):
-    
+    options = re.search('\(([^\)]*)\)', gr, re.IGNORECASE)
+    if options:
+        title = options.group(1)
+        for stuff in title.split('|'):
+            yield gr.replace("(" + title + ")", stuff)
+    else:
+        yield gr
+
 
 writer = open("pairs.tsv", "w+")
 
 with open("test.txt", "r") as input_file:
-    print "file opened"
+    logging.info("file opened")
+
     for line in input_file:
         for w in m.analyze(line):
-            
             if 'analysis' in w:
                 for item in w['analysis']:
-                    writer.write("\t".join([item['gr'], item['lex'], w['text']]).encode("utf-8") + "\n")
+                    for gramm_info in parse_gr(item['gr']):
+                        # print item['gr'], '->', gramm_info
+                        writer.write("\t".join(
+                            [gramm_info,
+                             item['lex'],
+                             w['text']]).encode("utf-8") + "\n")
 
 writer.close()
