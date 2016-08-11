d5bafcef571ed43baecccd13af67f0674b5e79ad
alexeyev <anton.m.alexeyev@gmail.com>
2016-08-11 03:06:26 +0300

    Pretty logging + stupid variable name bug

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ import data_helpers
 import model_all_stacked
 
 logging.basicConfig(filename='all_results.log',
-                    format='[%(asctime)s] %(name)s | %(levelname)s - %(message)s',
+                    format='[%(asctime)s] [%(levelname)s] %(message)s',
                     level=logging.DEBUG)
 
 lg = logging.getLogger("L")
 lg.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
 ch.setFormatter(formatter)
 lg.addHandler(ch)
 
@@ -28,7 +28,6 @@ np.random.seed(0123)  # for reproducibility
 # set parameters:
 
 subset = None
-
 save = False
 model_name_path = 'params/model.json'
 model_weights_path = 'params/model_weights.h5'
@@ -44,8 +43,6 @@ nb_epoch = 10
 lg.info('Loading data...')
 
 (x_train, y_train), (x_test, y_test) = data_helpers.load_relations()
-
-lg.info('Creating vocab...')
 vocab, reverse_vocab, vocab_size, check = data_helpers.create_vocab_set()
 
 lg.info('Build model...')
@@ -62,12 +59,11 @@ for e in xrange(nb_epoch):
 
     if subset:
         batches = data_helpers.mini_batch_generator(xi[:subset], yi[:subset],
-                                                    vocab, vocab_size, check,
-                                                    maxlen,
+                                                    vocab, vocab_size, check, maxlen,
                                                     batch_size=batch_size)
     else:
-        batches = data_helpers.mini_batch_generator(xi, yi, vocab, vocab_size,
-                                                    check, maxlen,
+        batches = data_helpers.mini_batch_generator(xi, yi,
+                                                    vocab, vocab_size, check, maxlen,
                                                     batch_size=batch_size)
 
     test_batches = data_helpers.mini_batch_generator(xi_test, yi_test, vocab,
@@ -78,7 +74,7 @@ for e in xrange(nb_epoch):
     step = 1
     start = datetime.datetime.now()
 
-    lg.info('Epoch: {}'.format(e))
+    lg.info('Epoch {} ---------------------'.format(e))
 
     for x_train, y_train, x_text_tr, y_text_tr in batches:
 
@@ -87,20 +83,21 @@ for e in xrange(nb_epoch):
         loss_avg = loss / step
 
         if step % 10 == 0:
-            lg.info('TRAINING Step: {} Loss: {}'.format(step, loss_avg))
+            lg.info('TRAINING step {}\tloss {}'.format(step, loss_avg))
         step += 1
 
     test_loss = 0.0
     test_step = 0
 
-    for x_test_batch, y_test_batch, x_text, y_text in test_batches:
+    lg.info("TESTING NOW ")
 
-        f_ev = model.test_on_batch(x_test_batch, x_test_batch)
+    for x_test_batch, y_test_batch, x_text, y_text in test_batches:
+        f_ev = model.test_on_batch(x_test_batch, y_test_batch)
         test_loss += f_ev
         test_loss_avg = test_loss / test_step
         test_step += 1
 
-        lg.info('TESTING step: {}, loss: {}'.format(test_step, test_loss_avg))
+        lg.info('TESTING step {}\tloss {}'.format(test_step, test_loss_avg))
         predicted_seq = model.predict(np.array([x_test_batch[0]]))
         lg.info(
             'Shapes x {} y_true {} y_pred {}'.format(
